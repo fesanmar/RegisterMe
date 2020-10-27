4470b850640a22f42ee74862cea1eff8fb28628e
Felipe <fesanmar@gmail.com>
2020-10-27 20:28:46 +0100

    Implements the app's main logic.

--- app/src/main/java/com/example/registerme/MainActivity.java
+++ app/src/main/java/com/example/registerme/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.registerme;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,11 +32,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     SwitchCompat swChildren;
     TextView lblMonitor;
 
+    HashMap<String, View> elements = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpViews();
+        fillElementsMap();
     }
 
     private void setUpViews() {
@@ -40,15 +50,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rgGender = findViewById(R.id.rgGender);
         swChildren = findViewById(R.id.swChildren);
         btnButton = findViewById(R.id.btnSave);
-        btnButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                click(view);
-            }
-        });
+        btnButton.setOnClickListener(this);
         btnClear = findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
         lblMonitor = findViewById(R.id.lblMonitor);
+    }
+
+    private void fillElementsMap() {
+        elements.put(getString(R.string.text_view_surname), txtSurname);
+        elements.put(getString(R.string.text_view_name), txtName);
+        elements.put(getString(R.string.text_view_age), txtAge);
+        elements.put(getString(R.string.text_view_gender), rgGender);
+        elements.put(getString(R.string.text_view_marital_status), spMaritalStatus);
+        elements.put(getString(R.string.any_children_switch), swChildren);
     }
 
     private void fillMaritalStatusSpinner() {
@@ -63,6 +77,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if (view.equals(btnClear))
             clearTextFields();
+        else
+            displayAccurateMessage();
     }
 
     public void clearTextFields() {
@@ -75,7 +91,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lblMonitor.setText("");
     }
 
-    public void click(View view) {
-        Toast.makeText(MainActivity.this, "Me pulsaste...", Toast.LENGTH_SHORT).show();
+    private void displayAccurateMessage() {
+        Collection<String> emptyFields = getMissingFields();
+        if (emptyFields.isEmpty())
+            showOutputAndClearForm();
+        else
+            displayMissingFieldsError(emptyFields);
     }
+
+    private void showOutputAndClearForm() {
+        displaySuccessMessage();
+        clearTextFields();
+    }
+
+    private Collection<String> getMissingFields() {
+        List<String> missingFields = new ArrayList<>();
+        for (Map.Entry<String, View> pair : elements.entrySet()) {
+            if (isEmpty(pair.getValue()))
+                missingFields.add(pair.getKey());
+        }
+        return missingFields;
+    }
+
+    private boolean isEmpty(View view) {
+        if (view instanceof TextView)
+            return ((TextView) view).getText().toString().trim().isEmpty();
+        else if (view instanceof RadioGroup)
+            return ((RadioGroup) view).getCheckedRadioButtonId() == -1;
+        else
+            return false;
+    }
+
+    private void displaySuccessMessage() {
+        String surname = txtSurname.getText().toString();
+        String name = txtName.getText().toString();
+        String adult = (Integer.parseInt(txtAge.getText().toString())) >= 18
+                ? getString(R.string.adult)
+                : getString(R.string.under_age);
+        int id = rgGender.getCheckedRadioButtonId();
+        RadioButton g = findViewById(id);
+        String gender = g.getText().toString().toLowerCase();
+        String maritalStatus = spMaritalStatus.getSelectedItem().toString().toLowerCase();
+        String hasChildren = swChildren.isChecked()
+                ? getString(R.string.with_children)
+                : getString(R.string.no_children);
+
+        String message = getString(R.string.success_message,
+                surname, name, adult, gender, maritalStatus, hasChildren);
+
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void displayMissingFieldsError(Collection<String> missingFields) {
+        StringBuilder sb = new StringBuilder(getString(R.string.missing_fields_message));
+        for (String field : missingFields)
+            sb.append(", ").append(field);
+        lblMonitor.setText(sb.toString());
+    }
+
 }
